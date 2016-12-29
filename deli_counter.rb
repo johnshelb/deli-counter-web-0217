4d3bc1a74927ea3f87f8d2c32f17355fb11d9281
John Shelby <jsnd11@gmail.com>
2016-12-29 17:43:55 -0500

    ready for pastrami

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+def line(katz_deli)
+  if katz_deli.count==0
+    puts "The line is currently empty."
+  else numbered_line=[]
+    for i in (0...katz_deli.count)
+    numbered_line.push("#{i+1}. #{katz_deli[i]}")
+  end
+  numbered_line_str=numbered_line.join(" ")
+    puts "The line is currently: #{numbered_line_str}"
+end
+end
+
+def take_a_number(katz_deli,name)
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli.index(name)+1} in line."
+end
+
+
+def now_serving(katz_deli)
+  if katz_deli.count==0
+    puts "There is nobody waiting to be served!"
+else puts "Currently serving #{katz_deli.shift}."
+end
+end
